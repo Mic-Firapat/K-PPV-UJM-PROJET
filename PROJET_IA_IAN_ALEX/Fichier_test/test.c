41ca393ac0f1e257efa30c8756877c619e7529dd
Mic <81635043+Mic-Firapat@users.noreply.github.com>
2021-05-07 01:04:07 +0200

    Add files via upload
    Tests des fonctions du module point.h

--- PROJET_IA_IAN_ALEX/Fichier_test/test.c
+++ PROJET_IA_IAN_ALEX/Fichier_test/test.c
@@ -5,7 +5,19 @@
 
 int main(){
 
-  printf("have fun avec les tests \n");
-
-  exit(1); /*Je sais que tu l'adores ça */
+    Point *p = creer_point();
+    Coord *c;
+    Point *pp = creer_point();
+    double largeur_grille= WIDTH/2. - WIDTH/100., hauteur_grille= HEIGHT/1.15 - HEIGHT/10.;
+    pxtopoint(pp, WIDTH/100 + 3 * largeur_grille/4. , HEIGHT/1.15);
+    p->x = -0.5;
+    p->y = 1;
+    p->classe = 666;
+    c = coordtopx(p);
+    printf("%f\n%f\n%d\n",pp->x,pp->y,p->classe);
+    printf("-----\n%d\n%f\n",WIDTH/100 + (WIDTH/2 - WIDTH/100)/2, HEIGHT/1.15);
+    printf("%d\n%d\n",c->x,c->y);
+    printf("%f\n",dist(p,pp));
+    printf("%f\n",sqrt(8));
+    exit(1);
 }
