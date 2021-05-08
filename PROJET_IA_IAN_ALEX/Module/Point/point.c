0e247dc4c78fd370e9879b41fce9eda0ee0c6fb2
Mic <81635043+Mic-Firapat@users.noreply.github.com>
2021-05-08 21:26:01 +0200

    Rajout de k_voisins dans le Module Point

--- PROJET_IA_IAN_ALEX/Module/Point/point.c
+++ PROJET_IA_IAN_ALEX/Module/Point/point.c
@@ -47,3 +47,68 @@ float distpx(Point *p1, Point *p2){
     return sqrt( pow(c2->x - c1->x,2) + pow(c2->y - c1->y, 2));
 }
 
+Point *ajout_point_tab(Point *t, Point *p, int *n){
+    (*n)++;
+    t = realloc(t, sizeof(Point) * (*n));
+    t[(*n)-1] = *p;
+    return t;
+}
+
+Point *retrait_point_tab(Point *t, int *n){
+    (*n)--;
+    t = realloc(t, sizeof(Point) * (*n));
+    return t;
+}
+
+
+float *distances(Point *t, Point *p, int n){
+    /*Fonction pas dans le .h*/
+    /*Renvoie un tableau de flottant contenant les distances entres les points.
+      Par exemple d[0] = dist(t[0],p)
+    Fonction utilisée dans k_voisins()*/
+    float *d = malloc(sizeof(float) * n);
+    int i;
+
+    for (i=0; i < n; i++){
+        d[i] = dist( &(t[i]), p);
+    }
+
+    return d;
+}
+
+
+int *k_voisins(Point *t, Point *p, int k, int n){
+    /*Ne surtout pas appeler avec un tableau de point vide ou un k inférieur à 1*/
+
+    float *d = distances(t,p,n);
+    int *voisins = malloc(sizeof(int) * (k+1));
+    int i,j,taille_voisins = 0, imax=0;
+    float dmax =  -1;
+    for (i = 0; i < n ; i++){
+        if (taille_voisins < k && d[i] != dist(p,p)){
+            voisins[taille_voisins+1] = i;
+            taille_voisins++;
+            if (dmax < d[i]){
+                dmax = d[i];
+                imax = i+1;
+            }
+        }
+        else {
+            if (d[i] != dist(p,p)){
+                if (dmax > d[i]){
+                    voisins[imax] = i;
+                    dmax = -1;
+                    for (j = 1; j < k +1; j++){
+                        if (dmax < d[voisins[j]]){
+                            dmax = d[voisins[j]];
+                            imax = j+1;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    voisins[0]=voisins[imax];
+    return voisins;
+}
+
--- PROJET_IA_IAN_ALEX/Module/Point/point.h
+++ PROJET_IA_IAN_ALEX/Module/Point/point.h
@@ -26,5 +26,22 @@ float dist(Point *p1, Point *p2);
 
 float distpx(Point *p1, Point *p2);
 /*Renvoie la distance entre deux points avec un flottant mais en pixels pour l'affichage du cercle*/
-    
+
+Point *ajout_point_tab(Point *t, Point *p, int *n);
+/*Ajoute un point p à la fin du tableau t en allouant la mémoire de manière dynamique. n est la taille du tableau qui est incrémentée automatiquement
+SURTOUT PAS INCREMENTER N EN DEHORS DE CETTE FONCTION
+Renvoie t*/
+
+Point *retrait_point_tab(Point *t, int *n);
+/*Retire le dernier point du tableau t et réalloue la mémoire nécessaire au tableau t
+n = taille du tableau avant retrait
+Retourne la tableau t ET PAS LE POINT ENLEVE
+Décrémente la valeur de n automatiquement
+SURTOUT PAS DECREMENTER N EN DEHORS*/
+
+int *k_voisins(Point *t, Point *p, int k, int n);
+/*Renvoie un tableaux d'entiers contenant les indices des k-PPV du point p dans le tableau t
+le premier élément du tableau correspond à l'indice du voisin le plus éloigné. Utilisé pour tracer le cercle
+Si p est déjà dans le tableau t, il est ignoré dans les voisins
+n = taille du tableau t (p compris si il s'y trouve)*/
 #endif
