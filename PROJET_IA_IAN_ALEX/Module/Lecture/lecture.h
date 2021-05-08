bb8ffcffa5f9416c97309cc4cec8fdfab35d1aff
Mic <81635043+Mic-Firapat@users.noreply.github.com>
2021-05-08 16:14:27 +0200

    Update lecture.h

--- PROJET_IA_IAN_ALEX/Module/Lecture/lecture.h
+++ PROJET_IA_IAN_ALEX/Module/Lecture/lecture.h
@@ -4,7 +4,7 @@
 #include "../necessaire.h"
 #include "../Point/point.h"
 
-Point *lire_fichier(char *nom_fichier, int *n, int *nbclasses);
+Point *chargement_fichier(char *nom_fichier, int *n, int *nbclasses);
 /*Renvoie le tableau de points t si lecture des données réussie et NULL sinon. 
 Modifie le contenu de l'entier n (nombre de points) et de l'entier nbclasses
 Attention, le nom_fichier est le chemin menant au fichier à partir du répertoire de l'exécutable*/
